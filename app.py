699e0de514ff27ebfa73c6af885c4ab521afdf11
crvftsmvn <bitruss@gmail.com>
2025-03-04 21:54:25 -0500

    Test website

--- app.py
+++ app.py
@@ -439,6 +439,6 @@ def get_data(league):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    # Check if running on Heroku
+    # Use the PORT environment variable provided by Render
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
